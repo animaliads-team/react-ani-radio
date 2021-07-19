2140e30023d921f5820daf241e14a11e41fa42ba
animaliads-team <ds.animalia@gmail.com>
2021-07-19 11:03:35 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ani-link-1mnah3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ani-link-1mnah3)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@animaliads/css-tokens/global-tokens.css';
+import '@animaliads/animalia-brand/theme.css';
+import '@animaliads/ani-radio';
+import './style.css';
+
+export default function App() {
+  return (
+    <ani-radio>
+      <span class="label">Google</span>
+    </ani-radio>
+  );
+}
